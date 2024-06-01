50abe59eda929d1ece77bc24023eec9eec73837d
dj-datta <166595376+dj-datta@users.noreply.github.com>
2024-06-01 10:23:57 +0530

    tt

--- demo/alb.tf
+++ demo/alb.tf
@@ -1,14 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
 resource "aws_lb" "test" {
   name               = "test-lb-tf"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
+  security_groups    = ["sg-0b97c932cb4f000dc"]
+  subnets            = ["subnet-06f7cfc0f810e5532"]
 
   enable_deletion_protection = true
 
   access_logs {
-    bucket  = aws_s3_bucket.lb_logs.id
+    bucket  = arn:aws:s3:::mybuckdj
     prefix  = "test-lb"
     enabled = true
   }
