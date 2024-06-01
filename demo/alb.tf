cc2479b618ce7c440eeda6fb6a6cf07e7b78d82b
dj-datta <166595376+dj-datta@users.noreply.github.com>
2024-06-01 10:03:57 +0530

    hh

--- demo/alb.tf
+++ demo/alb.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "test" {
-  name               = "lb-tf"
+  name               = "test-lb-tf"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
