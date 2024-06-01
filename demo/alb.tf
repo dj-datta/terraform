663048ab35327ead62b298f64d7aefeea68d7a9e
dj-datta <166595376+dj-datta@users.noreply.github.com>
2024-06-01 19:02:39 +0530

    s

--- demo/alb.tf
+++ demo/alb.tf
@@ -1,12 +1,13 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 resource "aws_lb" "test" {
   name               = "test-lb-tf"
   internal           = false
   load_balancer_type = "application"
   security_groups    = ["sg-0b97c932cb4f000dc"]
-  subnets            = ["subnet-06f7cfc0f810e5532"]
+  subnets            = ["subnet-06f7cfc0f810e5532"] ["subnet-06f73cc51b3c96d64"]
 
   enable_deletion_protection = true
 
