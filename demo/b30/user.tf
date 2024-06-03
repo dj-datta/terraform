85d107d2919fafcea63279d84c09791c97d64d2e
dj-datta <166595376+dj-datta@users.noreply.github.com>
2024-06-03 13:01:49 +0530

    dd

---
+++ demo/b30/user.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_iam_user" "example" {
+  name          = "example"
+  path          = "/"
+  force_destroy = true
+}
+
+resource "aws_iam_user_login_profile" "example" {
+  user    = datta
+  pgp_key = "keybase:some_person_that_exists"
+}
+
+output "password" {
+  value = User@123
+}
