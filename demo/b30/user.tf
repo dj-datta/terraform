749a3604646e41c31819f3382e6c173d85c7b248
dj-datta <166595376+dj-datta@users.noreply.github.com>
2024-06-03 15:43:22 +0530

    d

--- demo/b30/user.tf
+++ demo/b30/user.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_iam_user" "lb" {
-  name = "dj"
+  name = "hema"
   path = "/"
 
 }
@@ -23,22 +23,4 @@ resource "aws_iam_user_policy" "lb_ro" {
   name   = "AdministratorAccess"
   user   = aws_iam_user.lb.name
   policy = data.aws_iam_policy_document.lb_ro.json
-}
-resource "aws_iam_user" "dj" {
-  name          = "dj"
-  path          = "/"
-  force_destroy = true
-}
-
-resource "aws_iam_user_login_profile" "lb_ro" {
-  user    = "dj"
-  pgp_key = "keybase:some_person_that_exists"
-}
-
-output "password" {
-  value = "$Hema@123$"
-}
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
 }
